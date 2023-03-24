7a26c674d472d28e74945486d3e04699e37fae2d
SirJohanot <pavelkrafft@gmail.com>
2023-03-24 18:22:22 +0300

    completed the task

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (Stream<String> fileLines = Files.lines(file.toPath())) {
+            List<String> linesList = fileLines.collect(Collectors.toList());
+
+            String name = linesList.get(0)
+                    .replace("Name: ", "");
+
+            int age = Integer.parseInt(linesList.get(1)
+                    .replace("Age: ", ""));
+
+            String email = linesList.get(2)
+                    .replace("Email: ", "");
+
+            long phoneNumber = Long.parseLong(linesList.get(3)
+                    .replace("Phone: ", ""));
+
+            return new Profile(name, age, email, phoneNumber);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
